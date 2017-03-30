41b99fc9ed8e10e91def9ba882be7a5f50818d81
phischdev <phisch@oulook.de>
2017-03-30 16:48:08 +0200

    read csv, import libraries

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,1 +1,8 @@
+import csv
+from sklearn.linear_model import Rich
+import numpy
 
+with open('train.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print (', '.join(row))
