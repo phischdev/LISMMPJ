a010ff0a3a584c4b8430c4c0aad05057fa7ed8b9
phischdev <phisch@oulook.de>
2017-03-30 17:13:09 +0200

    read csv into list of lists

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,8 +1,15 @@
 import csv
-from sklearn.linear_model import Rich
+from sklearn.linear_model import Ridge
 import numpy
+
+data = []
 
 with open('train.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    next(spamreader)
     for row in spamreader:
-        print (', '.join(row))
+        head, *tail = row;
+        data.append((head, tail));
+        # print (', '.join(row))
+
+data.clear();
