47aaf95b855464b78c259c54612f18ca6f71a879
QSshellon <qs.michelle.woon@gmail.com>
2017-03-31 00:23:41 +0200

    added grid search

--- Task1/task1_qs.py
+++ Task1/task1_qs.py
@@ -1,6 +1,5 @@
-import sys
 from sklearn.kernel_ridge import KernelRidge
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error
 import random
 from sklearn.linear_model import Ridge
@@ -27,7 +26,7 @@ RMSE = mean_squared_error(y_test, y_pred)**0.5
 print('linear ridge regression error: ' + repr(RMSE))
 
 
-# prediction with kernel ridge regression
+# prediction with kernel ridge regression, 3rd degree polynomial
 clf_kp3 = KernelRidge(alpha=1.0, coef0=1, degree=3, kernel='poly')
 clf_kp3.fit(X_train, y_train)
 # predict for validation
@@ -37,7 +36,7 @@ RMSE = mean_squared_error(y_test, y_pred)**0.5
 print('Kernel (3rd degree poly) ridge regression error: ' + repr(RMSE))
 
 
-# prediction with kernel ridge regression
+# prediction with kernel ridge regression, 4th degree polynomial
 clf_kp4 = KernelRidge(alpha=1.0, coef0=1, degree=4, kernel='poly')
 clf_kp4.fit(X_train, y_train)
 # predict for validation
@@ -47,12 +46,66 @@ RMSE = mean_squared_error(y_test, y_pred)**0.5
 print('Kernel (4th degree poly) ridge regression error: ' + repr(RMSE))
 
 
+# NOTE: doesn't really help any more than 4th degree polynomial
+'''
+# prediction with kernel ridge regression, 5th degree polynomial
+clf_kp5 = KernelRidge(alpha=1.0, coef0=1, degree=4, kernel='poly')
+clf_kp5.fit(X_train, y_train)
+# predict for validation
+y_pred = clf_kp5.predict(X_test)
+# error
+RMSE = mean_squared_error(y_test, y_pred)**0.5
+print('Kernel (5th degree poly) ridge regression error: ' + repr(RMSE))
+'''
+
+
+# NOTE: is a bit worse off than grid search with kernel ridge regression, poly 3rd° most of the time
+# takes a long time to compute, so I'm commenting thing part out
+'''
+# grid search with kernel ridge regression, rbf
+gs_rbf = GridSearchCV(KernelRidge(kernel='rbf', gamma=1.0), cv=9, param_grid={"alpha": [1e0, 0.1, 1e-2, 1e-3],
+                                                                          "gamma": numpy.logspace(-2, 2, 5)})
+gs_rbf.fit(X_train, y_train)
+# predict for validation
+y_pred = gs_rbf.predict(X_test)
+# error
+RMSE = mean_squared_error(y_test, y_pred)**0.5
+print('grid search with kernel ridge regression (rbf) error: ' + repr(RMSE))
+'''
+
+
+# grid search with kernel ridge regression, poly 3rd°
+gs_kp3 = GridSearchCV(KernelRidge(kernel='poly', degree=3), cv=9, param_grid={"alpha": [1e0, 0.1, 1e-2, 1e-3],
+                                                                          "gamma": numpy.logspace(-2, 2, 5)})
+gs_kp3.fit(X_train, y_train)
+# predict for validation
+y_pred = gs_kp3.predict(X_test)
+# error
+RMSE = mean_squared_error(y_test, y_pred)**0.5
+print('grid search with kernel ridge regression (poly 3rd°) error: ' + repr(RMSE))
+
+
+# NOTE: is a bit worse off than grid search with kernel ridge regression, poly 3rd° most of the time
+# takes a long time to compute, so I'm commenting thing part out
+'''
+# grid search with kernel ridge regression, poly 4th°
+gs_kp4 = GridSearchCV(KernelRidge(kernel='poly', degree=4), cv=9, param_grid={"alpha": [1e0, 0.1, 1e-2, 1e-3],
+                                                                          "gamma": numpy.logspace(-2, 2, 5)})
+gs_kp4.fit(X_train, y_train)
+# predict for validation
+y_pred = gs_kp4.predict(X_test)
+# error
+RMSE = mean_squared_error(y_test, y_pred)**0.5
+print('grid search with kernel ridge regression (poly 4th°) error: ' + repr(RMSE))
+'''
+
+
 # predict for hand-in
 test = numpy.genfromtxt('test.csv', delimiter=',')
 test1 = test[1:]
 test2 = numpy.array(list(map(lambda li: li[1:], test1)))
 
-prediction = clf_kp3.predict(test2)
+prediction = gs_kp3.predict(test2)
 
 # write to file
 i = 900
