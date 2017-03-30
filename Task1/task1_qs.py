07424ef53325e5ea9054d9915de156dadee6dfea
QSshellon <qs.michelle.woon@gmail.com>
2017-03-30 19:51:55 +0200

    compared linear ridge regression with kernel (polynomial of 3rd degree) ridge regression and kernel (poly 4th degree) ridge regression

---
+++ Task1/task1_qs.py
@@ -0,0 +1,69 @@
+import sys
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import random
+from sklearn.linear_model import Ridge
+import numpy
+
+# load data
+data = numpy.genfromtxt('train.csv', delimiter=',')
+data = data[1:]
+
+xs = numpy.array(list(map(lambda li: li[2:], data)))
+ys = numpy.array(list(map(lambda li: li[1], data)))
+
+# split data
+r = random.randint(0, 4294967295) # generate a random number
+X_train, X_test, y_train, y_test = train_test_split(xs, ys, test_size=0.1, random_state=r)
+
+# prediction using linear ridge regression
+clf_ridge = Ridge(alpha=1.0)
+clf_ridge.fit(X_train, y_train)
+# predict for validation
+y_pred = clf_ridge.predict(X_test)
+# error
+RMSE = mean_squared_error(y_test, y_pred)**0.5
+print('linear ridge regression error: ' + repr(RMSE))
+
+
+# prediction with kernel ridge regression
+clf_kp3 = KernelRidge(alpha=1.0, coef0=1, degree=3, kernel='poly')
+clf_kp3.fit(X_train, y_train)
+# predict for validation
+y_pred = clf_kp3.predict(X_test)
+# error
+RMSE = mean_squared_error(y_test, y_pred)**0.5
+print('Kernel (3rd degree poly) ridge regression error: ' + repr(RMSE))
+
+
+# prediction with kernel ridge regression
+clf_kp4 = KernelRidge(alpha=1.0, coef0=1, degree=4, kernel='poly')
+clf_kp4.fit(X_train, y_train)
+# predict for validation
+y_pred = clf_kp4.predict(X_test)
+# error
+RMSE = mean_squared_error(y_test, y_pred)**0.5
+print('Kernel (4th degree poly) ridge regression error: ' + repr(RMSE))
+
+
+# predict for hand-in
+test = numpy.genfromtxt('test.csv', delimiter=',')
+test1 = test[1:]
+test2 = numpy.array(list(map(lambda li: li[1:], test1)))
+
+prediction = clf_kp3.predict(test2)
+
+# write to file
+i = 900
+prediction2 = []
+for row in prediction:
+    array = []
+    array.append(i)
+    array.append(row)
+    prediction2.append(array)
+    i += 1
+
+numpy.savetxt('output.csv', prediction2, fmt="%.15f", delimiter=',', header="Id,y", comments='')
+
+#print(data)
