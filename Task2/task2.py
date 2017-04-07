9314ab3731c738db4287ef10c2e9d06bee8474c8
jvkalle <jvkalle@student.ethz.ch>
2017-04-07 13:20:34 +0200

    lasso

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,16 +1,19 @@
 import numpy as np
 import pandas as pd
-df = pd.read_csv('train_csv', header = 0)
+df = pd.read_csv('train.csv', header = 0)
 df = df._get_numeric_data()
 
-dft = pd.read_csv('test_csv', header = 0)
+dft = pd.read_csv('test.csv', header = 0)
 dft = dft._get_numeric_data()
 
 x_train = df.ix[:,'x1':]
 y_train = df['y']
 x_test = dft.ix[:,'x1':]
 
-y_pred = []
+
+
+
+y_pred = y_train
 
 
 d = {'y': y_pred, 'Id': np.linspace(900, 900+y_pred.size-1, num=y_pred.size)}
