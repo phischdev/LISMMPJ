fb304305611eddea86d25b155fa03684d30f1e2b
niederbm <madphon@hotmail.com>
2017-05-02 17:20:45 +0200

    Data reading works

--- Task3/script.py
+++ Task3/script.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import tables as tb
 
 
-df = pd.read_hdf('train.csv', 'train')
+df = pd.read_hdf('train.h5', 'train')
 df = df._get_numeric_data()
 
-dft = pd.read_hdf('test.csv', 'test')
+dft = pd.read_hdf('test.h5', 'test')
 dft = dft._get_numeric_data()
 
 X_train = df.ix[:,'x1':].as_matrix();
