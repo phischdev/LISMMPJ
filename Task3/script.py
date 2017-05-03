a7c304ef48f85cb882bf73de2abfc5c8de76f28e
niederbm <madphon@hotmail.com>
2017-05-03 16:30:56 +0200

    Tensorflow two layers

--- Task3/script.py
+++ Task3/script.py
@@ -1,14 +1,134 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import pandas as pd
 import numpy as np
 import tables as tb
+import tensorflow as tf
+
+from tensorflow.contrib import learn
+from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+def cnn_model_fn(features, labels, mode):
+  """Model function for CNN."""
+  # Input Layer
+  input_layer = tf.reshape(features, [-1, 1, 100, 1])
+
+  # Convolutional Layer #1
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[1, 20],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #1
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[1, 2], strides=2)
+
+  # Convolutional Layer #2 and Pooling Layer #2
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[1, 2], strides=2)
+
+  # Dense Layer
+  pool2_flat = tf.reshape(pool2, [-1, 1 * 25 * 64])
+  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+  dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == learn.ModeKeys.TRAIN)
+
+  # Logits Layer
+  logits = tf.layers.dense(inputs=dropout, units=5)
+
+  loss = None
+  train_op = None
+
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  if mode != learn.ModeKeys.INFER:
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=5)
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=onehot_labels, logits=logits)
+
+  # Configure the Training Op (for TRAIN mode)
+  if mode == learn.ModeKeys.TRAIN:
+    train_op = tf.contrib.layers.optimize_loss(
+        loss=loss,
+        global_step=tf.contrib.framework.get_global_step(),
+        learning_rate=0.001,
+        optimizer="SGD")
+
+  # Generate Predictions
+  predictions = {
+      "classes": tf.argmax(
+          input=logits, axis=1),
+      "probabilities": tf.nn.softmax(
+          logits, name="softmax_tensor")
+  }
+
+  # Return a ModelFnOps object
+  return model_fn_lib.ModelFnOps(
+      mode=mode, predictions=predictions, loss=loss, train_op=train_op)
 
 
-df = pd.read_hdf('train.h5', 'train')
-df = df._get_numeric_data()
+def main(unused_argv):
+  # Load training and eval data
+  df = pd.read_hdf('train.h5', 'train')
+  df = df._get_numeric_data()
 
-dft = pd.read_hdf('test.h5', 'test')
-dft = dft._get_numeric_data()
+  dft = pd.read_hdf('test.h5', 'test')
+  dft = dft._get_numeric_data()
 
-X_train = df.ix[:,'x1':].as_matrix();
-y_train = df['y']
-x_test = dft.ix[:,'x1':].as_matrix();
+  X_all = df.as_matrix();
+  y_all = X_all[:,0]
+  X_all = X_all[:,1:]
+  #85% index
+  i = int(X_all.shape[0]*0.85)
+  X_train = X_all[:i,:]
+  X_eval = X_all[i:,:]
+  y_train = y_all[:i]
+  y_eval = y_all[i:]
+  x_test = dft.ix[:, 'x1':].as_matrix();
+  train_data = X_train # Returns np.array
+  train_labels = y_train
+  eval_data = X_eval # Returns np.array
+  eval_labels = y_eval
+
+  # Create the Estimator
+  mnist_classifier = learn.Estimator(
+      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+
+  # Set up logging for predictions
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+      tensors=tensors_to_log, every_n_iter=50)
+
+  # Train the model
+  mnist_classifier.fit(
+      x=train_data,
+      y=train_labels,
+      batch_size=100,
+      steps=20000,
+      monitors=[logging_hook])
+
+  # Configure the accuracy metric for evaluation
+  metrics = {
+      "accuracy":
+          learn.MetricSpec(
+              metric_fn=tf.metrics.accuracy, prediction_key="classes"),
+  }
+
+  # Evaluate the model and print results
+  eval_results = mnist_classifier.evaluate(
+      x=eval_data, y=eval_labels, metrics=metrics)
+  print(eval_results)
+
+
+if __name__ == "__main__":
+  tf.app.run()
+
